59ad208ff8710e36ae27ede0437a51db607a347d
nagarajRPoojari <np137270@gmail.com>
2025-03-23 20:29:17 +0530

    base setup

---
+++ internal/broker/register.go
@@ -0,0 +1,7 @@
+package broker
+
+func (t *Broker) Register(bucket Bucket[any]) error {
+	//lh, _ := lighthouse.GetLightHouse()
+	// lh.RegisterFollower(lighthouse.Path{bucket.Path()})
+	return nil
+}
