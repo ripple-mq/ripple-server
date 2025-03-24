f04f011d7cc31b3a00764aca87383c8170096c58
nagaraj poojari <116948655+nagarajRPoojari@users.noreply.github.com>
2025-03-24 15:50:55 +0530

    [PATCH] added create-bucker internal RPC (#15)
    * added create-bucker internal RPC
    
    * dummy commit

--- internal/broker/broker_test.go
+++ internal/broker/broker_test.go
@@ -10,7 +10,6 @@ package broker_test
 
 // 	"github.com/charmbracelet/log"
 // 	"github.com/ripple-mq/ripple-server/internal/broker"
-// 	"github.com/ripple-mq/ripple-server/internal/lighthouse"
 // 	"github.com/ripple-mq/ripple-server/pkg/p2p/encoder"
 // 	"github.com/ripple-mq/ripple-server/pkg/p2p/transport/tcp"
 // 	"github.com/ripple-mq/ripple-server/pkg/utils"
@@ -19,12 +18,9 @@ package broker_test
 // type AskQuery struct {
 // 	Count int
 // 	ID    string
-// }
+////}
 
 // func TestCreateAndRunQueue(t *testing.T) {
-
-// 	lh, _ := lighthouse.GetLightHouse()
-// 	lh.Connect()
 
 // 	b := broker.NewBroker()
 // 	paddr, caddr := utils.RandLocalAddr(), utils.RandLocalAddr()
--- server/internal/create.go
+++ server/internal/create.go
@@ -3,9 +3,33 @@ package server
 import (
 	"context"
 
+	"github.com/ripple-mq/ripple-server/internal/broker"
+	"github.com/ripple-mq/ripple-server/internal/lighthouse"
+	"github.com/ripple-mq/ripple-server/pkg/utils"
 	pb "github.com/ripple-mq/ripple-server/server/internal/proto"
 )
 
-func (t Server) CreateBucket(context.Context, *pb.CreateBucketReq) (*pb.CreateBucketResp, error) {
-	return nil, nil
+type PCServer struct {
+	Paddr string
+	Caddr string
+}
+
+func (t Server) CreateBucket(ctx context.Context, req *pb.CreateBucketReq) (*pb.CreateBucketResp, error) {
+
+	b := broker.NewBroker()
+	paddr, caddr := utils.RandLocalAddr(), utils.RandLocalAddr()
+	b.CreateAndRunQueue(paddr, caddr)
+
+	registerAndStartWatching(req.Topic, req.Bucket, paddr, caddr)
+
+	return &pb.CreateBucketResp{Success: true}, nil
+}
+
+// TODO: Avoid re-registering topic/bucket
+// TODO: Cron job to push messages in batches to read replicas from leader
+func registerAndStartWatching(topic string, bucket string, paddr string, caddr string) {
+	lh, _ := lighthouse.GetLightHouse()
+	path := lh.RegisterFollower(lighthouse.Path{Base: topic, Name: bucket}, PCServer{Paddr: paddr, Caddr: caddr})
+	lh.ElectLeader(path, PCServer{Paddr: paddr, Caddr: caddr})
+	lh.WatchForLeader(path, PCServer{Paddr: paddr, Caddr: caddr})
 }
