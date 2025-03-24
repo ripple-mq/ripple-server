3ca6438c0ff607f80efd4661bccbbcf6c5ccea5b
nagaraj poojari <116948655+nagarajRPoojari@users.noreply.github.com>
2025-03-24 14:52:58 +0530

    [PATCH] minor changes (#14)
    * minor changes
    
    * remove broker-test for now

---
+++ internal/broker/queue/server/consumer/core.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/charmbracelet/log"
+	"github.com/ripple-mq/ripple-server/internal/lighthouse"
+	"github.com/ripple-mq/ripple-server/pkg/p2p/encoder"
+)
+
+func (t *ConsumerServer[T]) startAcceptingConsumeReq() {
+	go func() {
+		for {
+			var query AskQuery
+			clientAddr, err := t.server.Consume(encoder.GOBDecoder{}, &query)
+			if err != nil {
+				continue
+			}
+			go t.handleConsumeReq(query, clientAddr)
+		}
+	}()
+}
+
+// TODO: offset will go wrong once i introduce TTL
+func (t *ConsumerServer[T]) handleConsumeReq(query AskQuery, clientAddr string) {
+	lh, _ := lighthouse.GetLightHouse()
+	data, _ := lh.Read(lighthouse.Path{Base: fmt.Sprintf("%s/%s", ConsumerPath, query.ID)})
+	offset, _ := strconv.Atoi(string(data))
+	defer lh.UpdateZnode(getConsumerPath(query.ID), strconv.Itoa(offset+query.Count))
+
+	for {
+		messages := t.q.SubArray(offset, offset+query.Count)
+		if len(messages) == 0 {
+			continue
+		}
+		if err := t.server.Send(clientAddr, struct{}{}, messages); err != nil {
+			log.Errorf("failed to send: %v", err)
+		}
+		offset += len(messages)
+	}
+}
+
+func onAcceptingConsumer(conn net.Conn, id []byte) {
+	var ID string
+	err := encoder.GOBDecoder{}.Decode(bytes.NewBuffer(id), &ID)
+	if err != nil {
+		log.Warnf("error reading data: %v", err)
+	}
+	log.Infof("Accepting consumer: %s %s", conn.RemoteAddr(), ID)
+	registerConsumer(ID)
+}
+
+func registerConsumer(id string) {
+	lh, _ := lighthouse.GetLightHouse()
+	lh.RegisterSequential(getConsumerPath(id), strconv.Itoa(0))
+}
+
+func getConsumerPath(id string) lighthouse.Path {
+	return lighthouse.Path{Base: fmt.Sprintf("%s/%s", ConsumerPath, id)}
+}
