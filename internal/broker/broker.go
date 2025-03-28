3f30e791e8b151f4cd15c975ae31049fc6c20216
nagaraj poojari <116948655+nagarajRPoojari@users.noreply.github.com>
2025-03-28 22:22:14 +0530

    bootstrap create bucket RPC (#31)

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -4,12 +4,19 @@ import (
 	"bytes"
 
 	"github.com/charmbracelet/log"
+	"github.com/ripple-mq/ripple-server/internal/broker/consumer/loadbalancer"
 	"github.com/ripple-mq/ripple-server/internal/broker/server"
 	"github.com/ripple-mq/ripple-server/internal/lighthouse"
+	"github.com/ripple-mq/ripple-server/internal/lighthouse/utils"
 	lu "github.com/ripple-mq/ripple-server/internal/lighthouse/utils"
 	tp "github.com/ripple-mq/ripple-server/internal/topic"
 	"github.com/ripple-mq/ripple-server/pkg/p2p/encoder"
+	"github.com/ripple-mq/ripple-server/pkg/utils/config"
 )
+
+type InternalRPCServerAddr struct {
+	Addr string
+}
 
 // PCServerAddr holds Pub/Sub server addresses
 type PCServerAddr struct {
@@ -78,4 +85,32 @@ func (t *Broker) RunCleanupLoop(server *server.Server, ch <-chan struct{}) {
 // TODO: Spin up cron job to distribute messages to follower in batches
 func onBecommingLeader(path lu.Path) {
 	log.Infof("Heyyyyy, I became leader: %v", path)
+}
+
+//  /servers
+//		/s-0000000001010101202nsa02 internal  gRPC address
+
+func (t *Broker) CreateBucket() ([]InternalRPCServerAddr, error) {
+	servers, err := t.getAllServers()
+	if err != nil {
+		return nil, err
+	}
+	nodes := []InternalRPCServerAddr{}
+	lb := loadbalancer.NewReadReqLoadBalancer()
+	for range config.Conf.Topic.Replicas {
+		var node InternalRPCServerAddr
+
+		encoder.GOBDecoder{}.Decode(bytes.NewBuffer(servers[lb.GetIndex(len(servers))]), node)
+		nodes = append(nodes, node)
+	}
+	return nodes, nil
+}
+
+func (t *Broker) getAllServers() ([][]byte, error) {
+	lh := lighthouse.GetLightHouse()
+	data, err := lh.ReadAllChildsData(utils.PathBuilder{}.Base(utils.Root()).CD("servers").Create())
+	if err != nil || len(data) == 0 {
+		return nil, err
+	}
+	return data, nil
 }
