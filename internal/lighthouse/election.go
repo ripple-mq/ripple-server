13f9583582511036f22f17bc2785c270b05a1e31
nagarajRPoojari <np137270@gmail.com>
2025-03-28 18:16:29 +0530

    added docs for lighthouse

--- internal/lighthouse/election.go
+++ internal/lighthouse/election.go
@@ -4,6 +4,14 @@ import (
 	"github.com/ripple-mq/ripple-server/internal/lighthouse/utils"
 )
 
+// StartElectLoop starts election loop and watching leader
+//
+//	Async
+//
+// Parameters:
+//   - path(utils.Path): Base path containing followers & leader
+//   - data(any): data to be written once becoming leader
+//   - func(path utils.Path): function to execute once becoming leader (Async)
 func (t *LigthHouse) StartElectLoop(path utils.Path, data any, onBecommingLeader func(path utils.Path)) <-chan struct{} {
 	t.elector.Start(path, data)
 	go func() {
@@ -14,6 +22,15 @@ func (t *LigthHouse) StartElectLoop(path utils.Path, data any, onBecommingLeader
 	return t.elector.ListenForFatalSignal()
 }
 
+// RegisterAsFollower registers `data` to followers directory
+//
+// Parameters:
+//   - path(utils.Path): Base path
+//   - data(any): data to be written
+//
+// Returns:
+//   - uitls.Path
+//   - error
 func (t *LigthHouse) RegisterAsFollower(path utils.Path, data any) (utils.Path, error) {
 	path, err := t.elector.RegisterFollower(path, data)
 	if err != nil {
@@ -22,6 +39,14 @@ func (t *LigthHouse) RegisterAsFollower(path utils.Path, data any) (utils.Path, 
 	return path, nil
 }
 
+// ReadLeader reads `data` from leader directory
+//
+// Parameters:
+//   - path(utils.Path): Base path
+//
+// Returns:
+//   - []byte
+//   - error
 func (t *LigthHouse) ReadLeader(path utils.Path) ([]byte, error) {
 	data, err := t.elector.ReadLeader(path)
 	if err != nil {
@@ -30,6 +55,14 @@ func (t *LigthHouse) ReadLeader(path utils.Path) ([]byte, error) {
 	return data, nil
 }
 
+// ReadFollowers reads data from followers
+//
+// Parameters:
+//   - path(utils.Path): Base path
+//
+// Returns:
+//   - [][]byte
+//   - error
 func (t *LigthHouse) ReadFollowers(path utils.Path) ([][]byte, error) {
 	data, err := t.elector.ReadFollowers(path)
 	if err != nil {
