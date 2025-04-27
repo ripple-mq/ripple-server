78a3d741fdd2f4dcaafe6a12781c0fc631a70f93
nagarajRPoojari <np137270@gmail.com>
2025-04-27 11:04:43 +0530

    minor performance improvement

--- server/exposed/create.go
+++ server/exposed/create.go
@@ -29,7 +29,7 @@ func (c Server) CreateBucket(ctx context.Context, req *pb.CreateBucketReq) (*pb.
 
 func createReq(servers []broker.InternalRPCServerAddr, topic topic.TopicBucket) {
 
-	for _, addr := range servers[:2] {
+	for _, addr := range servers[:1] {
 		func(addr string) {
 			conn, err := grpc.NewClient(addr, grpc.WithInsecure())
 			if err != nil {
